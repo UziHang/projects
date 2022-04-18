acf122dc5685d09b51bd698da0b801acccf5d60d
wu zihang <wuzihang1993@gmail.com>
2022-04-19 01:41:49 +0800

    bird 阴影学习

--- projects/bird/App.jsx
+++ projects/bird/App.jsx
@@ -1,11 +1,12 @@
 import React, { useRef } from "react";
-import { PresentationControls } from "@react-three/drei";
+import { PresentationControls,ContactShadows} from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 //  react three hook便于获取three场景元素
 import { useThree } from "@react-three/fiber";
-
+// 坐标辅助
+import { GizmoHelper } from "@react-three/drei";
+import { GizmoViewport } from "@react-three/drei";
 import Model from "./Model";
-import AutoModel from './AutoModel';
 const Brid = () => {
   return (
     // 定义渲染场景
@@ -23,7 +24,7 @@ const Brid = () => {
         camera={{
           // 配置摄像头
           fov: 25, //视角
-          position: [0, 0, 0], // 摄像头位置
+          position: [2, 2, 0], // 摄像头位置 xyz
         }}
         resize={{
           scroll: true,
@@ -34,12 +35,14 @@ const Brid = () => {
         }}
       >
         {/* 背景色 */}
-        <color attach="background" args={["#e0b7ff"]} />
+        {/* <color attach="background" args={["#fff"]} /> */}
         {/* 环境光源   颜色          强度 */}
-        <ambientLight color="#fff" intensity={0.5} />
+        <ambientLight color="#fff" intensity={0.1} />
         {/*  点光光源   */}
-        <pointLight color="red" position={[10, 10, 10]} />
-        {/* 渲染的物体 */}
+        <pointLight color="#fff" position={[10, 10, 10]} />
+
+        {/* 接触地面阴影    resolution；分辨率  */}
+        <ContactShadows opacity={1} scale={5} blur={1} far={5} resolution={32} color="#000000" />
 
         {/* 展示控制 */}
         <PresentationControls
@@ -48,13 +51,34 @@ const Brid = () => {
           speed={5} //灵敏性级别控制
           zoom={0.5} //最大缩放级别
           rotation={[0, 0, 0]} //模型默认旋转角度
-          // polar={[-Infinity, Infinity]} //垂直极限控制
-          // azimuth={[-Math.PI / 4, Math.PI / 4]} //水平极限控制
-          config={{ mass: 0.5, tension: 170, friction: 100 }} // Spring的配置=》质量 张力 摩擦力
+          polar={[0,0]} //垂直极限控制
+          azimuth={[-Math.PI / 4, Math.PI / 6]} //水平极限控制
+          config={{ mass: 0.5, tension: 170, friction: 20 }} // Spring的配置=》质量 张力 摩擦力
         >
-          {/* <Model /> */}
-          < AutoModel/>
+        <Model scale={0.0005}  position={[0,0.02,0]} 
+        onPointerMissed={()=>{console.log('动画结束');}}/>
         </PresentationControls>
+
+         {/* 视角辅助器 */}
+        <GizmoHelper
+          alignment="bottom-left" // widget alignment within scene
+          margin={[80, 80]} // widget margins (X, Y)
+          onUpdate={() => {
+            /* called during camera animation  */
+          }}
+          onTarget={() => {
+            /* return current camera target (e.g. from orbit controls) to center animation */
+          }}
+          renderPriority={() => {
+            /* use renderPriority to prevent the helper from disappearing if there is another useFrame(..., 1)*/
+          }}
+        >
+          <GizmoViewport
+            axisColors={["red", "green", "yellow"]}
+            labelColor="black"
+          />
+          {/* alternative: <GizmoViewcube /> */}
+        </GizmoHelper>
       </Canvas>
     </div>
   );
--- projects/bird/AutoModel.jsx
+++
@@ -1,8 +0,0 @@
-import { useGLTF } from "@react-three/drei";
-
-export default function AutoModel() {
-  const gltf = useGLTF("/projectsAssets/brid/models/brid-in-snow.glb", true);
-  console.log(gltf);
-  //primitive 原始
-  return <primitive object={gltf.scene} dispose={null} scale={0.001} />;
-}
