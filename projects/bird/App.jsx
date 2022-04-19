8dbbc88b4f19d7cb019b66736414520ccda27e4d
吴 梓航 <wuzh@yanhuamedia.com>
2022-04-19 12:23:51 +0800

    服务器渲染获取usergent报错测试

--- projects/bird/App.jsx
+++ projects/bird/App.jsx
@@ -1,13 +1,15 @@
-import React, { useRef } from "react";
-import { PresentationControls,ContactShadows} from "@react-three/drei";
+import React, { useEffect, useRef,useState } from "react";
+import { PresentationControls,OrbitControls,ContactShadows,GizmoHelper,GizmoViewport} from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 //  react three hook便于获取three场景元素
 import { useThree } from "@react-three/fiber";
-// 坐标辅助
-import { GizmoHelper } from "@react-three/drei";
-import { GizmoViewport } from "@react-three/drei";
+import isPC from '../../public/js/util';
+
 import Model from "./Model";
+
+
 const Brid = () => {
+  const [isPc,setIsPc]=useState(isPC)
   return (
     // 定义渲染场景
     <div
@@ -24,7 +26,7 @@ const Brid = () => {
         camera={{
           // 配置摄像头
           fov: 25, //视角
-          position: [2, 2, 0], // 摄像头位置 xyz
+          position: [0, 1, 2], // 摄像头位置
         }}
         resize={{
           scroll: true,
@@ -35,31 +37,43 @@ const Brid = () => {
         }}
       >
         {/* 背景色 */}
-        {/* <color attach="background" args={["#fff"]} /> */}
+        <color attach="background" args={["gray"]} />
         {/* 环境光源   颜色          强度 */}
-        <ambientLight color="#fff" intensity={0.1} />
+        <ambientLight color="#fff" intensity={0.3} />
         {/*  点光光源   */}
-        <pointLight color="#fff" position={[10, 10, 10]} />
+        <pointLight color="#fff" position={[-5, 5,5]} />
+        {/* 渲染的物体 */}
 
         {/* 接触地面阴影    resolution；分辨率  */}
-        <ContactShadows opacity={1} scale={5} blur={1} far={5} resolution={32} color="#000000" />
+        {/* <ContactShadows
+          opacity={1}
+          scale={5}
+          blur={1}
+          far={5}
+          resolution={32}
+          color="#000000"
+        /> */}
 
         {/* 展示控制 */}
         <PresentationControls
           global //全局旋转或者拖拽模型
-          cursor //是否在拖动上切换光标样式
+          // cursor //是否在拖动上切换光标样式
           speed={5} //灵敏性级别控制
           zoom={0.5} //最大缩放级别
           rotation={[0, 0, 0]} //模型默认旋转角度
           polar={[0,0]} //垂直极限控制
-          azimuth={[-Math.PI / 4, Math.PI / 6]} //水平极限控制
-          config={{ mass: 0.5, tension: 170, friction: 20 }} // Spring的配置=》质量 张力 摩擦力
+          azimuth={[-Infinity, Infinity]} //水平极限控制
+          config={{ mass: 1, tension: 100, friction: 50 }} // Spring的配置=》质量 张力 摩擦力
         >
-        <Model scale={0.0005}  position={[0,0.02,0]} 
-        onPointerMissed={()=>{console.log('动画结束');}}/>
-        </PresentationControls>
+          <Model
+            scale={isPc?0.0005:0.0002}
+            position={[0, -0.1, 0]}
+            onPointerMissed={() => {
+              console.log("动画结束");
+            }}
+          />
 
-         {/* 视角辅助器 */}
+           {/* 视角辅助器 */}
         <GizmoHelper
           alignment="bottom-left" // widget alignment within scene
           margin={[80, 80]} // widget margins (X, Y)
@@ -79,6 +93,9 @@ const Brid = () => {
           />
           {/* alternative: <GizmoViewcube /> */}
         </GizmoHelper>
+      
+         {/* <OrbitControls /> */}
+        </PresentationControls>
       </Canvas>
     </div>
   );
